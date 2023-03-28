5679121bcd577299ce96be85ee2d885e78f95709
NSavage13 <NS27@students.ptcollege.edu>
2023-03-28 18:33:51 -0400

    moved file

---
+++ UNO_GUI copy/src/Card.java
@@ -0,0 +1,56 @@
+
+
+class Card {
+    private String color; // "R", "B", "G", or "Y"
+    private String value;
+    private int direction; // 1 or -1
+    private int draw; // 0, 2 or 4
+    private int skip; // 0 or 1
+
+    // constructor
+    public Card(String color, String value, int direction, int draw, int skip) {
+        this.color = color;
+        this.value = value;
+        this.direction = direction;
+        this.draw = draw;
+        this.skip = skip;
+    }
+
+    // getter and setters for private fields
+
+    /** Return the color of the card
+     **/
+    public String getColor() {
+        return color;
+    }
+
+    /** Set the color of the card
+     **/
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    /** Return the value of the card
+     **/
+    public String getValue() {
+        return value;
+    }
+
+    /** Return the direction of the card
+     **/
+    public int getDirection () {
+        return direction;
+    }
+
+    /** Return the number of draws of the card
+     **/
+    public int getDraw () {
+        return draw;
+    }
+
+    /** Return the number of skips of the card
+     **/
+    public int getSkip () {
+        return skip;
+    }
+}
